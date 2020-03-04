1e596f87ef398d91315441841944ed27d1650f7c
Safo <s.i.post@mai.ru>
2020-03-05 00:19:46 +0500

    fix validInput

--- src/modules/validInput.js
+++ src/modules/validInput.js
@@ -1,13 +1,16 @@
 const validInput = () => {
   document.body.addEventListener('input', (event) => {
     const patternPhone = /^\+?[0-9]*$/;
+    const patternText = /^[А-Яа-яЁё ]*$/;
     if (event.target.type === 'tel') {
       if (!patternPhone.test(event.target.value)) {
         event.target.value = '';
       }
     }
     if (event.target.type === 'text' || event.target.placeholder === 'Ваше сообщение') {
-      event.target.value = event.target.value.replace(/[^А-ЯЁ а-яё]/, '');
+      if (!patternText.test(event.target.value)) {
+        event.target.value = '';
+      }
     }
   });
 };
